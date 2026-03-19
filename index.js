8c4be7e44c418a2003bfbb34e638518aa551637c
Dheeraj Patidar <131849451+Dheerajpatidar44@users.noreply.github.com>
2026-03-19 12:34:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const app = express();
 // ✅ Middleware
 app.use(
 	cors({
-		origin: ["http://localhost:5173"],
+		origin: ["http://localhost:5173" ,"https://ems-frontend-7bvphd3pj-dheerajpatidar44s-projects.vercel.app"],
 		credentials: true,
 		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
 		allowedHeaders: ["Content-Type", "Authorization"],
